b5f080a4a13d333510f39f04756516c6d2dbd816
stwaje <stwaje@mail.pima.edu>
2024-09-25 21:49:26 -0700

    Update cis129_lab02_shortStory.py
    Removed the hello world statement and added a short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+# AuthorL Sano Twaje
+# My first Python lab in my CIS129 course
+# Prints a short story
+print("One day a dove fell from a tree as a boy was walking by.")
+print("The boy picked up the dove and saw that it had injured its wing.")
+print("The dove was peciluar as it had glistening purple eyes and showed not fear.")
+print("It wing was injured so the boy brought dove home to nurse it until the wing healed.")
+print("It merely stared at the boy for the two weeks it stayed with him.")
+print("When its wing was healed. It looked at the boy said thank you, and flew away.")
+print("The boy never forgot that dove to this day.")
